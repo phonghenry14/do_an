fae721a47d1cb42fc47b4746f4f0c661528fcdca
phonghenry14 <tran.duc.phong@framgia.com>
2015-09-22 13:51:08 +0700

    messages

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   devise_scope :user do
-    get "sign_out", to: "devise/sessions#destroy"
-    get "sign_in", to: "devise/sessions#new"
+    get "sign_out", to: "sessions#destroy"
+    get "sign_in", to: "sessions#new"
     get "sign_up", to: "devise/registrations#new"
   end
   resources :users do
@@ -28,5 +28,9 @@ Rails.application.routes.draw do
         put "follow", to: "relationships#follow"
         put "unfollow", to: "relationships#unfollow"
     end
+  end
+
+  resources :conversations do
+    resources :messages
   end
 end
